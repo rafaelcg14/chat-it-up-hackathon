a2b08af55c12c4f63ef128f99b814c98cfd8390b
StarLordCG <rafaellord_123@outlook.com>
2024-10-31 12:38:47 -0500

    build: send to production

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-import '@fontsource/poppins';
-import '@fontsource/roboto';
-
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
 })
